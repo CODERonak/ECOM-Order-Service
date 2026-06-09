acc9c4dc6c93a8c76fad344f413a6d358f534aa9
r9988.2588 <r9988.2588@gmail.com>
2026-06-09 08:28:39 +0000

    fix: comment the http login

--- src/main/java/com/ecom/orderservice/security/SecurityConfig.java
+++ src/main/java/com/ecom/orderservice/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.ecom.orderservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,7 +25,7 @@ public class SecurityConfig {
                                                                 "/swagger-ui.html")
                                                 .permitAll()
                                                 .anyRequest().authenticated())
-                                .httpBasic(Customizer.withDefaults())
+                                // .httpBasic(Customizer.withDefaults())
                                 .formLogin(formLogin -> formLogin.disable())
                                 .sessionManagement(session -> session
                                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
